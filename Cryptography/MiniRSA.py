a4278a6a8ea048b27cf5e527697ab6819e16d04f
Selwyn Uy <107299589+Selwynuy@users.noreply.github.com>
2023-12-31 00:51:06 +0800

    Update MiniRSA.py

--- Cryptography/MiniRSA.py
+++ Cryptography/MiniRSA.py
@@ -1,5 +1,5 @@
 """
-Challenge Description:
+Description:
 What happens if you have a small exponent? There is a twist though, we padded the plaintext so that (M ** e) 
 is just barely larger than N. Let's decrypt this: ciphertext
 """

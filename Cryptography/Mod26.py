aba70a110244b0d8c8755015ce887ed7eacd3903
Selwyn Uy <107299589+Selwynuy@users.noreply.github.com>
2023-12-30 22:08:36 +0800

    Create Mod26.py

---
+++ Cryptography/Mod26.py
@@ -0,0 +1,12 @@
+"""
+Challenge Description:
+Cryptography can be easy, do you know what ROT13 is? cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_uJdSftmh}
+"""
+
+from codecs import encode
+
+text = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_uJdSftmh}"
+
+print(encode(text, 'rot_13'))
+
+#FLAG: picoCTF{next_time_I'll_try_2_rounds_of_rot13_hWqFsgzu}
